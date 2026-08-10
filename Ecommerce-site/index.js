b94843ca093a6a3a9120807ded6c07cd20a27ca7
jason jones <jasonjones@jasons-MacBook-Air.local>
2026-08-10 12:48:27 -0400

    new update

---
+++ Ecommerce-site/index.js
@@ -0,0 +1,15 @@
+function openMenu() {
+  document.body.classList.add("menu--open");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu--open");
+}
+function openMenu() {
+  document.body.classList.add("menu--open");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu--open");
+}
+index.html
